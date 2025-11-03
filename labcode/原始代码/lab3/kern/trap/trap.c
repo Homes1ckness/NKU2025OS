4a6f36f7c8ff2b2f0788afd8393a22bd6aa8c564
Chengyi Xiong <2693335820@qq.com>
2025-11-03 14:55:33 +0800

    update trap.c

--- labcode/原始代码/lab3/kern/trap/trap.c
+++ labcode/原始代码/lab3/kern/trap/trap.c
@@ -132,7 +132,7 @@ void interrupt_handler(struct trapframe *tf)
         // In fact, Call sbi_set_timer will clear STIP, or you can clear it
         // directly.
         // cprintf("Supervisor timer interrupt\n");
-        /* LAB3 EXERCISE1   YOUR CODE :  */
+        /* LAB3 EXERCISE1   2313310 :  */
         /*(1)设置下次时钟中断- clock_set_next_event()
          *(2)计数器（ticks）加一
          *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
@@ -211,7 +211,7 @@ void exception_handler(struct trapframe *tf)
         break;
     case CAUSE_BREAKPOINT:
         // 断点异常处理
-        /* LAB3 CHALLLENGE3   YOUR CODE :  */
+        /* LAB3 CHALLLENGE3   2310364 :  */
         /*(1)输出指令异常类型（ breakpoint）
          *(2)输出异常指令地址
          *(3)更新 tf->epc寄存器
