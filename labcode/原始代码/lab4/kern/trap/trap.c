48ed3d17a242bb18b09c7575172a87825f455b31
liuliuqueen <2310364@mail.nankai.edu.cn>
2025-11-18 19:39:17 +0800

    feat: lab4实验代码上传

--- labcode/原始代码/lab4/kern/trap/trap.c
+++ labcode/原始代码/lab4/kern/trap/trap.c
@@ -5,16 +5,15 @@
 #include <kdebug.h>
 #include <memlayout.h>
 #include <mmu.h>
-#include <stdio.h>
-#include <swap.h>
-#include <trap.h>
-#include <vmm.h>
 #include <riscv.h>
 #include <sbi.h>
+#include <stdio.h>
+#include <trap.h>
 
 #define TICK_NUM 100
 
-static void print_ticks() {
+static void print_ticks()
+{
     cprintf("%d ticks\n", TICK_NUM);
 #ifdef DEBUG_GRADE
     cprintf("End of Test.\n");
@@ -24,14 +23,15 @@ static void print_ticks() {
 
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S
  */
-void idt_init(void) {
-    /* LAB1 YOUR CODE : STEP 2 */
+void idt_init(void)
+{
+    /* LAB3 YOUR CODE : STEP 2 */
     /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
      *     All ISR's entry addrs are stored in __vectors. where is uintptr_t
      * __vectors[] ?
      *     __vectors[] is in kern/trap/vector.S which is produced by
      * tools/vector.c
-     *     (try "make" command in lab1, then you will find vector.S in kern/trap
+     *     (try "make" command in lab3, then you will find vector.S in kern/trap
      * DIR)
      *     You can use  "extern uintptr_t __vectors[];" to define this extern
      * variable which will be used later.
@@ -45,23 +45,23 @@ void idt_init(void) {
      * check the libs/x86.h to know more.
      *     Notice: the argument of lidt is idt_pd. try to find it!
      */
+
     extern void __alltraps(void);
-    /* Set sscratch register to 0, indicating to exception vector that we are
-     * presently executing in the kernel */
+    /* Set sup0 scratch register to 0, indicating to exception vector
+       that we are presently executing in the kernel */
     write_csr(sscratch, 0);
     /* Set the exception vector address */
     write_csr(stvec, &__alltraps);
-    set_csr(sstatus, SSTATUS_SIE);
-    /* Allow kernel to access user memory */
-    set_csr(sstatus, SSTATUS_SUM);
 }
 
 /* trap_in_kernel - test if trap happened in kernel */
-bool trap_in_kernel(struct trapframe *tf) {
+bool trap_in_kernel(struct trapframe *tf)
+{
     return (tf->status & SSTATUS_SPP) != 0;
 }
 
-void print_trapframe(struct trapframe *tf) {
+void print_trapframe(struct trapframe *tf)
+{
     cprintf("trapframe at %p\n", tf);
     print_regs(&tf->gpr);
     cprintf("  status   0x%08x\n", tf->status);
@@ -70,7 +70,8 @@ void print_trapframe(struct trapframe *tf) {
     cprintf("  cause    0x%08x\n", tf->cause);
 }
 
-void print_regs(struct pushregs *gpr) {
+void print_regs(struct pushregs *gpr)
+{
     cprintf("  zero     0x%08x\n", gpr->zero);
     cprintf("  ra       0x%08x\n", gpr->ra);
     cprintf("  sp       0x%08x\n", gpr->sp);
@@ -105,145 +106,166 @@ void print_regs(struct pushregs *gpr) {
     cprintf("  t6       0x%08x\n", gpr->t6);
 }
 
-static inline void print_pgfault(struct trapframe *tf) {
-    cprintf("page fault at 0x%08x: %c/%c\n", tf->badvaddr,
-            trap_in_kernel(tf) ? 'K' : 'U',
-            tf->cause == CAUSE_STORE_PAGE_FAULT ? 'W' : 'R');
-}
-
-static int pgfault_handler(struct trapframe *tf) {
-    extern struct mm_struct *check_mm_struct;
-    print_pgfault(tf);
-    if (check_mm_struct != NULL) {
-        return do_pgfault(check_mm_struct, tf->cause, tf->badvaddr);
-    }
-    panic("unhandled page fault.\n");
-}
-
-static volatile int in_swap_tick_event = 0;
-extern struct mm_struct *check_mm_struct;
-
-void interrupt_handler(struct trapframe *tf) {
+void interrupt_handler(struct trapframe *tf)
+{
     intptr_t cause = (tf->cause << 1) >> 1;
-    switch (cause) {
-        case IRQ_U_SOFT:
-            cprintf("User software interrupt\n");
-            break;
-        case IRQ_S_SOFT:
-            cprintf("Supervisor software interrupt\n");
-            break;
-        case IRQ_H_SOFT:
-            cprintf("Hypervisor software interrupt\n");
-            break;
-        case IRQ_M_SOFT:
-            cprintf("Machine software interrupt\n");
-            break;
-        case IRQ_U_TIMER:
-            cprintf("User software interrupt\n");
-            break;
-        case IRQ_S_TIMER:
-            // "All bits besides SSIP and USIP in the sip register are
-            // read-only." -- privileged spec1.9.1, 4.1.4, p59
-            // In fact, Call sbi_set_timer will clear STIP, or you can clear it
-            // directly.
-            // clear_csr(sip, SIP_STIP);
-            clock_set_next_event();
-            if (++ticks % TICK_NUM == 0) {
-                print_ticks();
+    switch (cause)
+    {
+    case IRQ_U_SOFT:
+        cprintf("User software interrupt\n");
+        break;
+    case IRQ_S_SOFT:
+        cprintf("Supervisor software interrupt\n");
+        break;
+    case IRQ_H_SOFT:
+        cprintf("Hypervisor software interrupt\n");
+        break;
+    case IRQ_M_SOFT:
+        cprintf("Machine software interrupt\n");
+        break;
+    case IRQ_U_TIMER:
+        cprintf("User Timer interrupt\n");
+        break;
+    case IRQ_S_TIMER:
+        // "All bits besides SSIP and USIP in the sip register are
+        // read-only." -- privileged spec1.9.1, 4.1.4, p59
+        // In fact, Call sbi_set_timer will clear STIP, or you can clear it
+        // directly.
+        // cprintf("Supervisor timer interrupt\n");
+        /* LAB3 EXERCISE1   2313310 :  */
+        /*(1)设置下次时钟中断- clock_set_next_event()
+         *(2)计数器（ticks）加一
+         *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
+         * (4)判断打印次数,当打印次数为10时，调用<sbi.h>中的关机函数关机
+         */
+        // (1) 设置下次时钟中断
+        clock_set_next_event();
+        // (2) 计数器加一
+        ticks++;
+        // (3) 每100次时钟中断打印一次
+        if (ticks % TICK_NUM == 0)
+        {
+            print_ticks();
+            // (4) 打印10次后关机
+            if (ticks / TICK_NUM == 10)
+            {
+                sbi_shutdown();
             }
-            break;
-        case IRQ_H_TIMER:
-            cprintf("Hypervisor software interrupt\n");
-            break;
-        case IRQ_M_TIMER:
-            cprintf("Machine software interrupt\n");
-            break;
-        case IRQ_U_EXT:
-            cprintf("User software interrupt\n");
-            break;
-        case IRQ_S_EXT:
-            cprintf("Supervisor external interrupt\n");
-            break;
-        case IRQ_H_EXT:
-            cprintf("Hypervisor software interrupt\n");
-            break;
-        case IRQ_M_EXT:
-            cprintf("Machine software interrupt\n");
-            break;
-        default:
-            print_trapframe(tf);
-            break;
+        }
+        break;
+    case IRQ_H_TIMER:
+        cprintf("Hypervisor software interrupt\n");
+        break;
+    case IRQ_M_TIMER:
+        cprintf("Machine software interrupt\n");
+        break;
+    case IRQ_U_EXT:
+        cprintf("User software interrupt\n");
+        break;
+    case IRQ_S_EXT:
+        cprintf("Supervisor external interrupt\n");
+        break;
+    case IRQ_H_EXT:
+        cprintf("Hypervisor software interrupt\n");
+        break;
+    case IRQ_M_EXT:
+        cprintf("Machine software interrupt\n");
+        break;
+    default:
+        print_trapframe(tf);
+        break;
     }
 }
 
+void exception_handler(struct trapframe *tf)
+{
+    switch (tf->cause)
+    {
+    case CAUSE_MISALIGNED_FETCH:
+        break;
+    case CAUSE_FAULT_FETCH:
+        break;
+    case CAUSE_ILLEGAL_INSTRUCTION:
+        // 非法指令异常处理
+        /* LAB3 CHALLENGE3   YOUR CODE :  */
+        /*(1)输出指令异常类型（ Illegal instruction）
+         *(2)输出异常指令地址
+         *(3)更新 tf->epc寄存器
+         */
+        // (1) 输出异常类型
+        cprintf("Exception type: Illegal instruction\n");
+        // (2) 输出异常指令地址
+        cprintf("Illegal instruction caught at 0x%08x\n", tf->epc);
+        // (3) 更新epc寄存器
+        // 检查指令长度：RISC-V压缩指令(C扩展)长度为2字节，标准指令长度为4字节
+        // 指令的最低2位如果不是11，则是压缩指令(16位)，否则是标准指令(32位)
+        unsigned int instruction = *(unsigned short *)tf->epc;
+        if ((instruction & 0x3) != 0x3)
+        {
+            tf->epc += 2; // 压缩指令，长度2字节
+        }
+        else
+        {
+            tf->epc += 4; // 标准指令，长度4字节
+        }
+        break;
+    case CAUSE_BREAKPOINT:
+        // 断点异常处理
+        /* LAB3 CHALLLENGE3   2310364 :  */
+        /*(1)输出指令异常类型（ breakpoint）
+         *(2)输出异常指令地址
+         *(3)更新 tf->epc寄存器
+         */
+        // (1) 输出异常类型
+        cprintf("Exception type: breakpoint\n");
+        // (2) 输出异常指令地址
+        cprintf("ebreak caught at 0x%08x\n", tf->epc);
+        // (3) 更新epc寄存器
+        // 检查指令长度：RISC-V压缩指令(C扩展)长度为2字节，标准指令长度为4字节
+        // ebreak可能是压缩指令c.ebreak(2字节)或标准ebreak(4字节)
+        unsigned int inst = *(unsigned short *)tf->epc;
+        if ((inst & 0x3) != 0x3)
+        {
+            tf->epc += 2; // 压缩指令，长度2字节
+        }
+        else
+        {
+            tf->epc += 4; // 标准指令，长度4字节
+        }
+        break;
+    case CAUSE_MISALIGNED_LOAD:
+        break;
+    case CAUSE_FAULT_LOAD:
+        break;
+    case CAUSE_MISALIGNED_STORE:
+        break;
+    case CAUSE_FAULT_STORE:
+        break;
+    case CAUSE_USER_ECALL:
+        break;
+    case CAUSE_SUPERVISOR_ECALL:
+        break;
+    case CAUSE_HYPERVISOR_ECALL:
+        break;
+    case CAUSE_MACHINE_ECALL:
+        break;
+    default:
+        print_trapframe(tf);
+        break;
+    }
+}
 
-void exception_handler(struct trapframe *tf) {
-    int ret;
-    switch (tf->cause) {
-        case CAUSE_MISALIGNED_FETCH:
-            cprintf("Instruction address misaligned\n");
-            break;
-        case CAUSE_FETCH_ACCESS:
-            cprintf("Instruction access fault\n");
-            break;
-        case CAUSE_ILLEGAL_INSTRUCTION:
-            cprintf("Illegal instruction\n");
-            break;
-        case CAUSE_BREAKPOINT:
-            cprintf("Breakpoint\n");
-            break;
-        case CAUSE_MISALIGNED_LOAD:
-            cprintf("Load address misaligned\n");
-            break;
-        case CAUSE_LOAD_ACCESS:
-            cprintf("Load access fault\n");
-            if ((ret = pgfault_handler(tf)) != 0) {
-                print_trapframe(tf);
-                panic("handle pgfault failed. %e\n", ret);
-            }
-            break;
-        case CAUSE_MISALIGNED_STORE:
-            cprintf("AMO address misaligned\n");
-            break;
-        case CAUSE_STORE_ACCESS:
-            cprintf("Store/AMO access fault\n");
-            if ((ret = pgfault_handler(tf)) != 0) {
-                print_trapframe(tf);
-                panic("handle pgfault failed. %e\n", ret);
-            }
-            break;
-        case CAUSE_USER_ECALL:
-            cprintf("Environment call from U-mode\n");
-            break;
-        case CAUSE_SUPERVISOR_ECALL:
-            cprintf("Environment call from S-mode\n");
-            break;
-        case CAUSE_HYPERVISOR_ECALL:
-            cprintf("Environment call from H-mode\n");
-            break;
-        case CAUSE_MACHINE_ECALL:
-            cprintf("Environment call from M-mode\n");
-            break;
-        case CAUSE_FETCH_PAGE_FAULT:
-            cprintf("Instruction page fault\n");
-            break;
-        case CAUSE_LOAD_PAGE_FAULT:
-            cprintf("Load page fault\n");
-            if ((ret = pgfault_handler(tf)) != 0) {
-                print_trapframe(tf);
-                panic("handle pgfault failed. %e\n", ret);
-            }
-            break;
-        case CAUSE_STORE_PAGE_FAULT:
-            cprintf("Store/AMO page fault\n");
-            if ((ret = pgfault_handler(tf)) != 0) {
-                print_trapframe(tf);
-                panic("handle pgfault failed. %e\n", ret);
-            }
-            break;
-        default:
-            print_trapframe(tf);
-            break;
+static inline void trap_dispatch(struct trapframe *tf)
+{
+    if ((intptr_t)tf->cause < 0)
+    {
+        // interrupts
+        interrupt_handler(tf);
+    }
+    else
+    {
+        // exceptions
+        exception_handler(tf);
     }
 }
 
@@ -253,13 +275,8 @@ void exception_handler(struct trapframe *tf) {
  * the code in kern/trap/trapentry.S restores the old CPU state saved in the
  * trapframe and then uses the iret instruction to return from the exception.
  * */
-void trap(struct trapframe *tf) {
+void trap(struct trapframe *tf)
+{
     // dispatch based on what type of trap occurred
-    if ((intptr_t)tf->cause < 0) {
-        // interrupts
-        interrupt_handler(tf);
-    } else {
-        // exceptions
-        exception_handler(tf);
-    }
+    trap_dispatch(tf);
 }
